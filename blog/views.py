f88da5b629566c08f09aacdcb92c70ab8bb77a39
admin <k.fromlewicz@wp.pl>
2017-05-28 16:54:13 +0200

    Modyfikacja

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
+from .models import Post
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    post = Post.objects.filter().order_by('published_date')
+    return render(request, 'blog/post_list.html', {"posts":post})
